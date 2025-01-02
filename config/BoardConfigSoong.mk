ProjectBlaze/vendor_blaze

soong: Add camera package name
Use TARGET_CAMERA_PACKAGE_NAME to specify
the package name.

Suggested-By: Adithya R <gh0strider.2k18.reborn@gmail.com>
Change-Id: I9768d0ac40517b6555fd0606b299760f5baed71b
Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

config/BoardConfigSoong.mk
@@ -55,7 +55,8 @@ SOONG_CONFIG_blazeGlobalVars += \
     uses_egl_display_array \
     uses_legacy_fd_fbdev \
     uses_oplus_camera \
-    needs_camera_boottime
+    needs_camera_boottime \
+    target_camera_package_name
 
 SOONG_CONFIG_NAMESPACES += blazeNvidiaVars
 SOONG_CONFIG_blazeNvidiaVars += \
@@ -89,6 +90,7 @@ SOONG_CONFIG_blazeGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBD
 SOONG_CONFIG_blazeGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_blazeGlobalVars_sdmcore_has_is_display_hw_available_func := $(TARGET_SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC)
 SOONG_CONFIG_blazeGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
+SOONG_CONFIG_blazeGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0

TRR-labs/vendor_yaap

config/BoardConfigSoong.mk
@@ -39,7 +39,8 @@ SOONG_CONFIG_lineageGlobalVars += \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
     target_charge_rate_multiplier \
-    needs_camera_boottime
+    needs_camera_boottime \
+    target_camera_package_name
 
 SOONG_CONFIG_NAMESPACES += lineageQcomVars
 SOONG_CONFIG_lineageQcomVars += \
@@ -65,6 +66,7 @@ endef
 # Soong bool variables
 SOONG_CONFIG_lineageGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_lineageGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
+SOONG_CONFIG_lineageGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
 
 # Set default values
 TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib